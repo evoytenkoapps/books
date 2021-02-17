6a6b1e1609453d3d21be99f034c9bc79ffecbae2
Евгений Войтенко <jozik.service@gmail.com>
2021-02-17 13:04:38 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBookComponent } from './components/create-book/create-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBookComponent
   ],
   imports: [
     BrowserModule,
