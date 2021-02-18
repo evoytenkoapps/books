d9a6c0c7e8eb62a0e919288e7e73577d86a29e70
Евгений Войтенко <jozik.service@gmail.com>
2021-02-18 17:36:06 +0300

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CreateBookComponent } from "./components/create-book/create-book.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { KeysPipe } from "./pipes/keys.pipe";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule } from "@angular/common";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [AppComponent, CreateBookComponent, KeysPipe],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
